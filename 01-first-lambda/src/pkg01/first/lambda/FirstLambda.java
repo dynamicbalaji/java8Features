4e59588e5bc65a1144a53565ed690200dda3b413
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-10-08 16:13:22 -0500

    Adding changes with normal FileFilter interface

--- 01-first-lambda/src/pkg01/first/lambda/FirstLambda.java
+++ 01-first-lambda/src/pkg01/first/lambda/FirstLambda.java
@@ -5,17 +5,34 @@
  */
 package pkg01.first.lambda;
 
+import java.io.File;
+import java.io.FileFilter;
+
 /**
  *
- * @author Balaji_Kumar03
+ * @author Balaji Ashok Kumar (dynamicbalaji4u@gmail.com)
  */
 public class FirstLambda {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // Anonymous class for FileFilter
+        FileFilter fileFilter = new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                // If pathname ends with .docx, then pathname will be returned
+                return pathname.getName().endsWith(".docx");
+            }            
+        };
+                
+        File dir = new File("D:\\Murugan 70-480");        
+        
+        File[] filesLst = dir.listFiles(fileFilter);
+        
+        for(File file: filesLst){
+            System.out.println(file);
+        }
     }
     
 }
