b033ee110e85c632d8c79c936e11867709f13d6f
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-10-08 16:22:15 -0500

    Adding changes with normal Runnable interface

---
+++ 01-first-lambda/src/pkg01/first/lambda/RunnableLambda.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg01.first.lambda;
+
+/**
+ *
+ * @author Balaji Ashok Kumar (dynamicbalaji4u@gmail.com)
+ * @website http://www.balajiashokkumar.me
+ */
+public class RunnableLambda {
+    
+    public static void main(String[] args) throws InterruptedException {
+        Runnable runnable = new Runnable(){
+            @Override
+            public void run() {
+                for(int i=0;i<3;i++){
+                    System.out.println("Thread "+Thread.currentThread().getName());
+                }
+            }            
+        };
+        
+        Thread thread = new Thread(runnable);
+        thread.start();
+        thread.join();
+    }
+    
+}
